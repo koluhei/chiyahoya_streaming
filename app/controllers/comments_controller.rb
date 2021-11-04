09349383b68ed86638a40dcdd6b2670b81d4b550
koluhei <qpal79@gmail.com>
2021-11-04 13:04:45 +0900

    ルーティング、リンクの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
   def index
   end
+
+  def stream
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "comments#index"
+  resources :comments, only: [:index] do
+    collection do
+      get 'stream'
+    end
+  end
 end
